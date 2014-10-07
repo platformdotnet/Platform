bb69309d313dd57015b63ee2f286408972f3c267
tumtumtum <tumtumtum@gmail.com>
2014-10-07 18:20:02 +0100

    Added ObjectReferenceIdentityEqualityComparer

---
+++ src/Platform/ObjectReferenceIdentityEqualityComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Platform
+{
+	public class ObjectReferenceIdentityEqualityComparer<T>
+		: IEqualityComparer<T>
+		where T : class
+	{
+		public static readonly ObjectReferenceIdentityEqualityComparer<T> Default = new ObjectReferenceIdentityEqualityComparer<T>();
+
+		public bool Equals(T x, T y)
+		{
+			return object.ReferenceEquals(x, y);
+		}
+
+		public int GetHashCode(T obj)
+		{
+			if (obj == null)
+			{
+				throw new ArgumentNullException("obj");
+			}
+
+			return obj.GetHashCode();
+		}
+	}
+}
