ff545c8c617b5075c95e67bc50a7f4f8d497d236
maarten <maarten@picturpresent.nl>
2015-05-20 18:00:38 +0200

    Made formatting available for objects that implement IFormattable and use the StringableTypeSerializer

--- src/Platform.Xml.Serialization/StandardTypeSerializerFactory.cs
+++ src/Platform.Xml.Serialization/StandardTypeSerializerFactory.cs
@@ -5,217 +5,149 @@ using System.Collections;
 
 namespace Platform.Xml.Serialization
 {
-	public class StandardTypeSerializerFactory
-		: TypeSerializerFactory
-	{
-		private readonly SerializerOptions options;
+    public class StandardTypeSerializerFactory
+        : TypeSerializerFactory
+    {
+        private readonly SerializerOptions options;
 
-		public StandardTypeSerializerFactory(SerializerOptions options)
-		{
-			this.options = options;
-		}
+        public StandardTypeSerializerFactory(SerializerOptions options)
+        {
+            this.options = options;
+        }
 
-		public override TypeSerializer NewTypeSerializerBySupportedType(Type supportedType, TypeSerializerCache cache)
-		{
-			return NewTypeSerializerBySupportedType(supportedType, null, cache);
-		}
+        public override TypeSerializer NewTypeSerializerBySupportedType(Type supportedType, TypeSerializerCache cache)
+        {
+            return NewTypeSerializerBySupportedType(supportedType, null, cache);
+        }
 
-		public override TypeSerializer NewTypeSerializerBySupportedType(Type supportedType, SerializationMemberInfo memberInfo, TypeSerializerCache cache)
-		{
-			const string error = "A TypeSerializer can't be created for the given type without a memberInfo";
+        public override TypeSerializer NewTypeSerializerBySupportedType(Type supportedType, SerializationMemberInfo memberInfo, TypeSerializerCache cache)
+        {
+            const string error = "A TypeSerializer can't be created for the given type without a memberInfo";
 
-			if (typeof(Enum).IsAssignableFrom(supportedType))
-			{
-				if (memberInfo == null)
-				{
-					throw new XmlSerializerException(error);
-				}
+            if (typeof(Enum).IsAssignableFrom(supportedType))
+            {
+                return new EnumTypeSerializer(memberInfo, cache, options);
+            }
+            else if (typeof(IDictionary).IsAssignableFrom(supportedType))
+            {
+                return new DictionaryTypeSerializer(memberInfo, cache, options);
+            }
+            else if (typeof(Type).IsAssignableFrom(supportedType))
+            {
+                return new RuntimeTypeTypeSerializer(memberInfo, cache, options);
+            }
+            else if (supportedType == typeof(XmlNode))
+            {
+                return XmlNodeNodeTypeSerializer.Default;
+            }
+            else if (StringableTypeSerializer.SupportedTypes.Contains(supportedType))
+            {
+                return new StringableTypeSerializer(supportedType, memberInfo, cache, options);
+            }
+            else if (supportedType == typeof(Guid))
+            {
+                return new GuidSerializer(supportedType, memberInfo, cache, options);
+            }
+            else if (supportedType == typeof(Color))
+            {
+                return new ColorSerializer(supportedType, memberInfo, cache, options);
+            }
+            else if (supportedType == typeof(DateTime))
+            {
+                return new DateTimeTypeSerializer(memberInfo, cache, options);
+            }
+            else if (supportedType.IsGenericType && supportedType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                return NewTypeSerializerBySupportedType
+                (
+                    Nullable.GetUnderlyingType(supportedType),
+                    memberInfo,
+                    cache
+                );
+            }
+            else
+            {
+                var implementsList = false;
+                var implementsGenericList = false;
 
-				return new EnumTypeSerializer(memberInfo, cache, options);
-			}
-			else if (typeof(IDictionary).IsAssignableFrom(supportedType))
-			{
-				if (memberInfo == null)
-				{
-					throw new ArgumentNullException("memberInfo");
-				}
+                implementsList = typeof(IList).IsAssignableFrom(supportedType);
 
-				return new DictionaryTypeSerializer(memberInfo, cache, options);
-			}
-			else if (typeof(Type).IsAssignableFrom(supportedType))
-			{
-				return new RuntimeTypeTypeSerializer(memberInfo, cache, options);
-			}
-			else if (supportedType == typeof(XmlNode))
-			{
-				return XmlNodeNodeTypeSerializer.Default;
-			}
-			else if (supportedType == typeof(bool))
-			{
-				return StringableTypeSerializer.BoolSerializer;
-			}
-			else if (supportedType == typeof(byte))
-			{
-				return StringableTypeSerializer.ByteSerializer;
-			}
-			else if (supportedType == typeof(char))
-			{
-				return StringableTypeSerializer.CharSerializer;
-			}
-			else if (supportedType == typeof(decimal))
-			{
-				return StringableTypeSerializer.DecimalSerializer;
-			}
-			else if (supportedType == typeof(double))
-			{
-				return StringableTypeSerializer.DoubleSerializer;
-			}
-			else if (supportedType == typeof(float))
-			{
-				return StringableTypeSerializer.FloatSerializer;
-			}
-			else if (supportedType == typeof(int))
-			{
-				return StringableTypeSerializer.IntSerializer;
-			}
-			else if (supportedType == typeof(long))
-			{
-				return StringableTypeSerializer.LongSerializer;
-			}
-			else if (supportedType == typeof(sbyte))
-			{
-				return StringableTypeSerializer.SByteSerializer;
-			}
-			else if (supportedType == typeof(short))
-			{
-				return StringableTypeSerializer.ShortSerializer;
-			}
-			else if (supportedType == typeof(string))
-			{
-				return StringableTypeSerializer.StringSerializer;
-			}
-			else if (supportedType == typeof(uint))
-			{
-				return StringableTypeSerializer.UIntSerializer;
-			}
-			else if (supportedType == typeof(ulong))
-			{
-				return StringableTypeSerializer.ULongSerializer;
-			}
-			else if (supportedType == typeof(ushort))
-			{
-				return StringableTypeSerializer.UShortSerializer;
-			}
-			else if (supportedType == typeof(Guid))
-			{
-				return StringableTypeSerializer.GuidSerializer;
-			}
-			else if (supportedType == typeof(Color))
-			{
-				return StringableTypeSerializer.ColorSerializer;
-			}
-			else if (supportedType == typeof(DateTime))
-			{
-				if (memberInfo == null)
-				{
-					throw new ArgumentNullException("memberInfo");
-				}
+                implementsGenericList = supportedType.FindInterfaces
+                (
+                    (type, criterea) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(System.Collections.Generic.IList<>),
+                    null
+                ).Length > 0;
 
-				return new DateTimeTypeSerializer(memberInfo, cache, options);
-			}
-			else if (supportedType.IsGenericType
-				&& supportedType.GetGenericTypeDefinition() == typeof(Nullable<>))
-			{
-				return NewTypeSerializerBySupportedType
-				(
-					Nullable.GetUnderlyingType(supportedType),
-					memberInfo,
-					cache
-				);
-			}
-			else
-			{
-				var implementsList = false;
-				var implementsGenericList = false;
+                if (implementsList || implementsGenericList)
+                {
+                    if (memberInfo == null)
+                    {
+                        throw new XmlSerializerException(error);
+                    }
 
-				implementsList = typeof(IList).IsAssignableFrom(supportedType);
-				
-				implementsGenericList = supportedType.FindInterfaces
-				(
-					(type, criterea) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof (System.Collections.Generic.IList<>),
-					null
-				).Length > 0;
+                    return new ListTypeSerializer(memberInfo, cache, options);
+                }
 
-				if (implementsList || implementsGenericList)
-				{
-					if (memberInfo == null)
-					{
-						throw new XmlSerializerException(error);
-					}
+                return new ComplexTypeTypeSerializer(memberInfo, supportedType, cache, options);
+            }
+        }
 
-					return new ListTypeSerializer(memberInfo, cache, options);
-				}
+        public override TypeSerializer NewTypeSerializerBySerializerType(Type serializerType, TypeSerializerCache cache)
+        {
+            return NewTypeSerializerBySerializerType(serializerType, null, cache);
+        }
 
-				return new ComplexTypeTypeSerializer(memberInfo, supportedType, cache, options);
-			}			
-		}
+        public override TypeSerializer NewTypeSerializerBySerializerType(Type serializerType, SerializationMemberInfo memberInfo, TypeSerializerCache cache)
+        {
+            TypeSerializer retval = null;
 
-		public override TypeSerializer NewTypeSerializerBySerializerType(Type serializerType, TypeSerializerCache cache)
-		{
-			return NewTypeSerializerBySerializerType(serializerType, null, cache);
-		}
+            try
+            {
+                retval = (TypeSerializer)Activator.CreateInstance(serializerType, new object[] { cache, options });
+            }
+            catch (Exception)
+            {
+            }
 
-		public override TypeSerializer NewTypeSerializerBySerializerType(Type serializerType, SerializationMemberInfo memberInfo, TypeSerializerCache cache)
-		{
-			TypeSerializer retval = null;			
+            if (retval == null && memberInfo != null)
+            {
+                try
+                {
+                    retval = (TypeSerializer)Activator.CreateInstance(serializerType, new object[] { memberInfo, cache, options });
+                }
+                catch (Exception)
+                {
+                }
+            }
 
-			try
-			{
-				retval = (TypeSerializer)Activator.CreateInstance(serializerType, new object[] { cache, options });
-			}
-			catch (Exception)
-			{
-			}
-		
-			if (retval == null && memberInfo != null)
-			{
-				try
-				{
-					retval = (TypeSerializer)Activator.CreateInstance(serializerType, new object[] { memberInfo, cache, options });
-				}
-				catch (Exception)
-				{
-				}
-			}
+            if (retval == null && memberInfo != null)
+            {
+                try
+                {
+                    retval = (TypeSerializer)Activator.CreateInstance(serializerType, new object[] { memberInfo.ReturnType });
+                }
+                catch (Exception)
+                {
+                }
+            }
 
-			if (retval == null && memberInfo != null)
-			{
-				try
-				{
-					retval = (TypeSerializer)Activator.CreateInstance(serializerType, new object[] { memberInfo.ReturnType });
-				}
-				catch (Exception)
-				{
-				}
-			}
+            if (retval == null)
+            {
+                try
+                {
+                    retval = (TypeSerializer)Activator.CreateInstance(serializerType, new object[0]);
+                }
+                catch (Exception)
+                {
+                }
+            }
 
-			if (retval == null)
-			{
-				try
-				{
-					retval = (TypeSerializer)Activator.CreateInstance(serializerType, new object[0]);
-				}
-				catch (Exception)
-				{
-				}
-			}
-			
-			if (retval == null)
-			{
-				throw new XmlSerializerException("Unable to create TypeSerializer: " + serializerType.GetType().ToString());
-			}
+            if (retval == null)
+            {
+                throw new XmlSerializerException("Unable to create TypeSerializer: " + serializerType.GetType().ToString());
+            }
 
-			return retval;
-		}
-	}
+            return retval;
+        }
+    }
 }
