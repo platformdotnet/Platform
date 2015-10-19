fa2dda61eec1ca0b0f44adad69cb4d69b4cea2ea
Thong Nguyen <tumtumtum@gmail.com>
2015-10-19 17:44:39 +0100

    Added DictionaryExtensions

---
+++ src/Platform/Collections/DictionaryExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Platform.Collections
+{
+	public static class DictionaryExtensions
+	{
+		public static V ItemOrDefault<K, V>(this Dictionary<K, V> dictionary, K key)
+		{
+			V result;
+
+			if (dictionary.TryGetValue(key, out result))
+			{
+				return result;
+			}
+
+			return default(V);
+		}
+
+		public static V? ItemOrNull<K, V>(this Dictionary<K, V> dictionary, K key)
+			where V : struct
+		{
+			V result;
+
+			if (dictionary.TryGetValue(key, out result))
+			{
+				return result;
+			}
+
+			return null;
+		}
+	}
+}
