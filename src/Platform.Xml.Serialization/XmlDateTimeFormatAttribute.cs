4334ca040909417efeeab2666ffd5741c4ba0b03
Ethan.Zhang <OO.Snail@Gmail.com>
2015-11-30 21:21:06 +0800

    Add Culture and UseUserOverride string Property that defines the culturally appropriate format of displaying dates and times.  UseUserOverride indicating whether the current CultureInfo object uses the user-selected culture settings. Usage:[XmlDateTimeFormat("yyyy/MM/dd HH:ss:mm", false)], [XmlDateTimeFormat("yyyy/MM/dd HH:ss:mm", "zh-CN", false)]

--- src/Platform.Xml.Serialization/XmlDateTimeFormatAttribute.cs
+++ src/Platform.Xml.Serialization/XmlDateTimeFormatAttribute.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace Platform.Xml.Serialization
 {
@@ -6,15 +7,66 @@ namespace Platform.Xml.Serialization
 	public class XmlDateTimeFormatAttribute
 		: XmlSerializationAttribute
 	{
-		public string Format
-		{
-			get;
-			set;
-		}
 
-		public XmlDateTimeFormatAttribute(string format)
-		{
-			this.Format = format;
-		}
-	}
+        public string Format
+        {
+            get;
+            set;
+        }
+
+        private bool cultureSet = false;
+
+        public bool CultureSet
+        {
+            get { return cultureSet; }
+        }
+
+        private string culture;
+
+        public string Culture
+        {
+            get { return culture; }
+            set
+            {
+                cultureSet = true;
+                culture = value;
+            }
+        }
+
+        public bool UseUserOverride
+        {
+            get;
+            set;
+        }
+
+        public CultureInfo CultureInfo
+        {
+            get
+            {
+                return this.CultureSet ? new CultureInfo(this.Culture, this.UseUserOverride) : CultureInfo.CurrentCulture;
+            }
+        }
+
+        public XmlDateTimeFormatAttribute(string format)
+        {
+            this.Format = format;
+        }
+
+        public XmlDateTimeFormatAttribute(string format, string culture) : this(format)
+        {
+            this.Culture = culture;
+            this.UseUserOverride = true;
+        }
+        public XmlDateTimeFormatAttribute(string format, bool useUserOverride) : this(format)
+        {
+            this.Culture = CultureInfo.CurrentCulture.Name;
+            this.UseUserOverride = useUserOverride;
+        }
+
+        public XmlDateTimeFormatAttribute(string format, string culture, bool useUserOverride) : this(format)
+        {
+            this.Culture = culture;
+            this.UseUserOverride = useUserOverride;
+        }
+    }
 }
